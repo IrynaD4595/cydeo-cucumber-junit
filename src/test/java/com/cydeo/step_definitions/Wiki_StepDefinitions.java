60b758b467843cfb00bfed0bff67aeda5150ba2a
iryna <iryna.dubenets@yahoo.com>
2022-03-18 15:19:31 -0500

    wiki practice done

--- src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -36,4 +36,13 @@ public class Wiki_StepDefinitions {
 
     }
 
+    @Then("User sees {string} is in the main header")
+    public void userSeesSteveJobsIsInTheMainHeader(String string) {
+
+        Assert.assertTrue(wikiSearchPage.mainHeader.isDisplayed());
+
+        Assert.assertTrue(wikiSearchPage.mainHeader.getText().equals(string));
+
+
+    }
 }
